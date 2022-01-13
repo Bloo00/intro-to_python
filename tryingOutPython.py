8446995bd340f28b196a8d114f481738f8f236c0
Bloo00 <kabin.yangg@gmail.com>
2022-01-13 15:25:00 -0800

    new name funcrion

--- tryingOutPython.py
+++ tryingOutPython.py
@@ -5,4 +5,29 @@ def my_first_function(x,y,name="kabin"):
             
     print("x")
 
+students = [
+    { 
+        "name": "Kimmie",
+        "city": "Atlanta"
+    },
+    { 
+        "name": "Chris",
+        "city": "Salt Lake City"
+    },
+    { 
+        "name": "Zack",
+        "city": "Los Angeles"
+    }
+]
+
 my_first_function(4,4)
+
+
+def get_names(students):
+    result = []
+    for name in students:
+        result.append(name.get('name'))
+    return result
+
+print("Name List: ", get_names(students))
+    
