7bdf21537f1fb6937f0cdebcd0ca900c445d2b5c
Bloo00 <kabin.yangg@gmail.com>
2022-01-13 15:09:54 -0800

    hihi

--- tryingOutPython.py
+++ tryingOutPython.py
@@ -1,7 +1,8 @@
-def my_first_function():
-    for i in range(10):
-        for j in range (10):
-            print ("x")
-    print("w")
+def my_first_function(x,y,name="kabin"):
+    for i in range(x):
+        for j in range (y):
+            print ("y", name)
+            
+    print("x")
 
-my_first_function()
+my_first_function(4,4)
